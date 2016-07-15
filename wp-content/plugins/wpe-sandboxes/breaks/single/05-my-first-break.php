a3eb32af26afb97becb6126f0cecb2edf212e13f
Alex Zuniga <alex.zuniga@wpengine.com>
2016-07-15 10:23:15 -0500

    adding break

--- wp-content/plugins/wpe-sandboxes/breaks/single/05-my-first-break.php
+++ wp-content/plugins/wpe-sandboxes/breaks/single/05-my-first-break.php
@@ -12,6 +12,37 @@ class wpeBreak extends abstractBreak {
     }
 
     function breakSite() {
-        // Make awesome breaky things happen here!
+     //BEGIN CONFIG INJECTION
+        $path = get_home_path();
+        //set path to the config file
+        $wp_conf_path = $path . "wp-config.php";
+
+        // Get the contents of that file.
+        $wp_conf = file_get_contents($wp_conf_path);
+
+        // Look for DB_COLLATE line to put our stuff below.
+        $find = "#(DB_COLLATE.*)#";
+
+        // The $1 here is the match above (Just like HTML Post Processing!)
+
+        // The $ is escaped with a \ so PHP doesn't think it's a normal PHP variable.
+        $repl = "\$1\ndefine( 'WP_DEBUG_LOG', true );\ndefine( 'WP_DEBUG_DISPLAY', true );";
+
+        // Create a new variable with the results of our search-replace.
+        $wp_conf_new = preg_replace($find, $repl, $wp_conf);
+
+        // Output the result to the wp-config.php file. :)
+        file_put_contents($wp_conf_path, $wp_conf_new);
+
+      //BEGIN LOG INJECTION
+
+        $elog = fopen( $path . "wp-content/debug.log", "w") or die ("UNABLE TO OPEN FILE! :( ");
+
+        $message = "PAY YOUR DEVELOPER YOU CHEAP BASTARD!"
+
+        fwrite($elog, $message);
+
+        fclose($elog);
+
     }
 }
