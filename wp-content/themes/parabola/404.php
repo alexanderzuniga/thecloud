db82a87fad3bef2eb5b65b65489aa4101cf8d98f
Alex Zuniga <alex.zuniga@wpengine.com>
2016-12-12 15:51:09 -0600

    testing bypass lint push

--- wp-content/themes/parabola/404.php
+++ wp-content/themes/parabola/404.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The template for displaying 404 pages (Not Found).
- *adsglknasdgaslkghlkhglkjg
+ *lkajsdg;adsglknasdgaslkghlkhglkjg
  * @package Cryout Creations
  * @subpackage parabola
  * @since parabola 0.5
